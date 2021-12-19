4fd73b82451860c17ec1c8207a76245820bde922
hkk <827435393@qq.com>
2021-12-19 18:04:24 +0800

    update v1.2.0

--- hello.go
+++ hello.go
@@ -4,10 +4,10 @@ import "fmt"
 
 // Hello says hello.
 func Hello() {
-	fmt.Println("Hello go mod!")
+	fmt.Println("Hello go mod.v2!")
 }
 
 // Bye says bye.
 func Bye() {
-	fmt.Println("Bye go mod!")
+	fmt.Println("Bye go mod.v2!")
 }
